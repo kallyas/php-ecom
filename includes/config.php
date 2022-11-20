228e100c7b592df751148e5655de65e48ff802f6
kallyas <kallyasmedia@gmail.com>
2022-11-20 22:27:08 +0300

    ch: add products and config files

---
+++ includes/config.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once "DB.php";
+
+// get database connection
+$database = new DB();
+$db = $database->getConnection();
---
+++ includes/products.php
@@ -0,0 +1,245 @@
+<!-- File that handles products crud -->
+<?php
+
+
+class Product {
+    // Database connection and table name
+    private $conn;
+    private $table_name = "products";
+
+    // Object properties
+    public $id;
+    public $name;
+    public $description;
+    public $price;
+    public $category_id;
+    public $category_name;
+    public $timestamp;
+
+    // Constructor with $db as database connection
+    public function __construct($db) {
+        $this->conn = $db;
+    }
+
+    // Read products
+    function read() {
+        // Select all query
+        $query = "SELECT
+                    c.name as category_name, p.id, p.name, p.description, p.price, p.category_id, p.timestamp
+                FROM
+                    " . $this->table_name . " p
+                    LEFT JOIN
+                        categories c
+                            ON p.category_id = c.id
+                ORDER BY
+                    p.timestamp DESC";
+
+        // Prepare query statement
+        $stmt = $this->conn->prepare($query);
+
+        // Execute query
+        $stmt->execute();
+
+        return $stmt;
+    }
+
+    // Create product
+    function create() {
+        // Query to insert record
+        $query = "INSERT INTO
+                    " . $this->table_name . "
+                SET
+                    name=:name, price=:price, description=:description, category_id=:category_id, timestamp=:timestamp";
+
+        // Prepare query
+        $stmt = $this->conn->prepare($query);
+
+        // Sanitize
+        $this->name = htmlspecialchars(strip_tags($this->name));
+        $this->price = htmlspecialchars(strip_tags($this->price));
+        $this->description = htmlspecialchars(strip_tags($this->description));
+        $this->category_id = htmlspecialchars(strip_tags($this->category_id));
+        $this->timestamp = htmlspecialchars(strip_tags($this->timestamp));
+
+        // Bind values
+        $stmt->bindParam(":name", $this->name);
+        $stmt->bindParam(":price", $this->price);
+        $stmt->bindParam(":description", $this->description);
+        $stmt->bindParam(":category_id", $this->category_id);
+        $stmt->bindParam(":timestamp", $this->timestamp);
+
+        // Execute query
+        if ($stmt->execute()) {
+            return true;
+        }
+
+        return false;
+    }
+
+    // Used when filling up the update product form
+    function readOne() {
+        // Query to read single record
+        $query = "SELECT
+                    c.name as category_name, p.id, p.name, p.description, p.price, p.category_id, p.timestamp
+                FROM
+                    " . $this->table_name . " p
+                    LEFT JOIN
+                        categories c
+                            ON p.category_id = c.id
+                WHERE
+                    p.id = ?
+                LIMIT
+                    0,1";
+
+        // Prepare query statement
+        $stmt = $this->conn->prepare($query);
+
+        // Bind id of product to be updated
+        $stmt->bindParam(1, $this->id);
+
+        // Execute query
+        $stmt->execute();
+
+        // Get retrieved row
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        // Set values to object properties
+        $this->name = $row['name'];
+        $this->price = $row['price'];
+        $this->description = $row['description'];
+        $this->category_id = $row['category_id'];
+        $this->category_name = $row['category_name'];
+
+    }
+
+    // Update the product
+    function update() {
+        // Update query
+        $query = "UPDATE
+                    " . $this->table_name . "
+                SET
+                    name = :name,
+                    price = :price,
+                    description = :description,
+                    category_id = :category_id
+                WHERE
+                    id = :id";
+
+        // Prepare query statement
+        $stmt = $this->conn->prepare($query);
+
+        // Sanitize
+        $this->name = htmlspecialchars(strip_tags($this->name));
+        $this->price = htmlspecialchars(strip_tags($this->price));
+        $this->description = htmlspecialchars(strip_tags($this->description));
+        $this->category_id = htmlspecialchars(strip_tags($this->category_id));
+        $this->id = htmlspecialchars(strip_tags($this->id));
+
+        // Bind new values
+        $stmt->bindParam(':name', $this->name);
+        $stmt->bindParam(':price', $this->price);
+        $stmt->bindParam(':description', $this->description);
+        $stmt->bindParam(':category_id', $this->category_id);
+        $stmt->bindParam(':id', $this->id);
+
+        // Execute the query
+        if ($stmt->execute()) {
+            return true;
+        }
+
+        return false;
+    }
+
+    // Delete the product
+    function delete() {
+        // Delete query
+        $query = "DELETE FROM " . $this->table_name . " WHERE id = ?";
+
+        // Prepare query
+        $stmt = $this->conn->prepare($query);
+
+        // Sanitize
+        $this->id = htmlspecialchars(strip_tags($this->id));
+
+        // Bind id of record to delete
+        $stmt->bindParam(1, $this->id);
+
+        // Execute query
+        if ($stmt->execute()) {
+            return true;
+        }
+
+        return false;
+    }
+
+    // Search products
+    function search($keywords) {
+        // Select all query
+        $query = "SELECT
+                    c.name as category_name, p.id, p.name, p.description, p.price, p.category_id, p.timestamp
+                FROM
+                    " . $this->table_name . " p
+                    LEFT JOIN
+                        categories c
+                            ON p.category_id = c.id
+                WHERE
+                    p.name LIKE ? OR p.description LIKE ? OR c.name LIKE ?
+                ORDER BY
+                    p.timestamp DESC";
+
+        // Prepare query statement
+        $stmt = $this->conn->prepare($query);
+
+        // Sanitize
+        $keywords = htmlspecialchars(strip_tags($keywords));
+        $keywords = "%{$keywords}%";
+
+        // Bind
+        $stmt->bindParam(1, $keywords);
+        $stmt->bindParam(2, $keywords);
+        $stmt->bindParam(3, $keywords);
+
+        // Execute query
+        $stmt->execute();
+
+        return $stmt;
+    }
+
+    // Read products with pagination
+    public function readPaging($from_record_num, $records_per_page) {
+        // Select query
+        $query = "SELECT
+                    c.name as category_name, p.id, p.name, p.description, p.price, p.category_id, p.timestamp
+                FROM
+                    " . $this->table_name . " p
+                    LEFT JOIN
+                        categories c
+                            ON p.category_id = c.id
+                ORDER BY p.timestamp DESC
+                LIMIT ?, ?";
+
+        // Prepare query statement
+        $stmt = $this->conn->prepare($query);
+
+        // Bind variable values
+        $stmt->bindParam(1, $from_record_num, PDO::PARAM_INT);
+        $stmt->bindParam(2, $records_per_page, PDO::PARAM_INT);
+
+        // Execute query
+        $stmt->execute();
+
+        // Return values from database
+        return $stmt;
+    }
+
+    // Used for paging products
+    public function count() {
+        $query = "SELECT COUNT(*) as total_rows FROM " . $this->table_name . "";
+
+        $stmt = $this->conn->prepare($query);
+        $stmt->execute();
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        return $row['total_rows'];
+    }
+}
