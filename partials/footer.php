9fa11366e340e7fef81eae1be63329dfcad85d97
kallyas <kallyasmedia@gmail.com>
2022-11-21 11:08:11 +0300

    ch: add partials

---
+++ partials/footer.php
@@ -0,0 +1,8 @@
+<?php
+
+$js_dir = '../js/';
+
+?>
+<script src="<?php echo $js_dir . 'script.js' ?>"></script>
+</body>
+</html>
---
+++ partials/header.php
@@ -0,0 +1,15 @@
+<?php
+// css files are loaded from outside the public folder
+// so we need to go back one level
+$css_dir = '../css/';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="<?php echo $css_dir . 'style.css' ?>">
+    <script src="https://kit.fontawesome.com/0d67f6bb4c.js" crossorigin="anonymous"></script>
+</head>
