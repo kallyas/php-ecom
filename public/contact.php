2b8f1a3e07c36a9bbcef8bce739b6f6f7cd6ea7d
diphan-source <ahabwediphan@yahoo.com>
2022-11-21 13:38:16 +0300

    ch:add_contact

---
+++ public/contact.php
@@ -0,0 +1,92 @@
+<?php 
+require_once '../partials/header.php';
+?>
+<body>
+    <!-- Navigation -->
+    <header class="header">
+        <nav class="nav">
+            <div class="nav__logo">
+                <a href="index.php">
+                    Shop
+                </a>
+            </div>
+            <div class="nav__links">
+                <a href="index.php">Home</a>
+                <a href="about.php">About</a>
+                <a href="contact.php">Contact</a>
+            </div>
+        </nav>
+    </header>
+   <!--contact form-->
+    <section class="contact">
+         <div class="contact__container">
+              <div class="contact__title">
+                <h1>Contact Us</h1>
+              </div>
+              <div class="contact__form">
+                <form action="">
+                     <div class="contact__form__group">
+                          <label for="name">Name</label>
+                          <input type="text" name="name" id="name">
+                     </div>
+                     <div class="contact__form__group">
+                          <label for="email">Email</label>
+                          <input type="email" name="email" id="email">
+                     </div>
+                     <div class="contact__form__group">
+                          <label for="message">Message</label>
+                          <textarea name="message" id="message" cols="30" rows="10"></textarea>
+                     </div>
+                     <div class="contact__form__group">
+                          <button type="submit">Submit</button>
+                     </div>
+                </form>
+              </div>
+         </div>
+    </section>
+    <!-- Footer -->
+    <footer class="footer">
+        <div class="footer__container">
+            <div class="footer__links">
+                <div class="footer__links__item">
+                    <h2>Company</h2>
+                    <a href="#">About Us</a>
+                    <a href="#">Contact Us</a>
+                    <a href="#">Careers</a>
+                    <a href="#">Affiliate</a>
+                </div>
+                <div class="footer__links__item">
+                    <h2>Get Help</h2>
+                    <a href="#">FAQ</a>
+                    <a href="#">Shipping</a>
+                    <a href="#">Returns</a>
+                    <a href="#">Order Status</a>
+                    <a href="#">Payment Options</a>
+                </div>
+                <div class="footer__links__item">
+                    <h2>Online Shop</h2>
+                    <a href="#">Watch</a>
+                    <a href="#">Bag</a>
+                    <a href="#">Shoes</a>
+                    <a href="#">Dress</a>
+                </div>
+                <div class="footer__links__item">
+                    <h2>Policy</h2>
+                    <a href="#">Return Policy</a>
+                    <a href="#">Terms Of Use</a>
+                    <a href="#">Security</a>
+                    <a href="#">Privacy</a>
+                    <a href="#">Sitemap</a>
+                    <a href="#">Store Directory</a>
+                </div>
+            </div>
+            <div class="footer__social">
+                <a href="#"><i class="fa-brands fa-facebook-f"></i></a>
+                <a href="#"><i class="fa-brands fa-twitter"></i></a>
+                <a href="#"><i class="fa-brands fa-instagram"></i></a>
+                <a href="#"><i class="fa-brands fa-youtube"></i></a>
+                <a href="#"><i class="fa-brands fa-linkedin-in"></i></a>
+            </div>
+        </div>
+    </footer>
+<?php require_once '../partials/footer.php'; ?>
