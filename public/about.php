3f7a77d5466a1f9eb675610a70808e6b0d9a77d8
diphan-source <ahabwediphan@yahoo.com>
2022-12-01 16:56:03 +0300

    fix about

--- public/about.php
+++ public/about.php
@@ -1,10 +1,19 @@
-<?php 
+<?php
+session_start();
 require_once '../partials/header.php';
 require_once '../includes/products.php';
+require_once '../includes/config.php';
+
+
+$product = new Product($db);
+$products = $product->read();
+$featured = $product->read();
+
 ?>
+
 <body>
-     <!-- Navigation -->
-     <header class="header">
+    <!-- Navigation -->
+    <header class="header">
         <nav class="nav">
             <div class="nav__logo">
                 <a href="index.php">
@@ -18,25 +27,25 @@ require_once '../includes/products.php';
                 <a href="login.php">Login</a>
             </div>
         </nav>
-     </header>
-    
+    </header>
+
     <!--About-->
-        <section class="about">
-            <div class="about__container">
-                <div class="about__title">
-                    <h2>Welcome to Our Ecommerence shop</h2>
-                </div>
-                <div class="about__subtitle">
-                    <h3>shopping made easy </h3>
-                </div>
-                <div class="about__info">
-                    <p>We are simply an online store , looking forward to make 
-                        shopping easy for people with in Uganda and beyond.
-                        <em>Shop</em> is a platform where you can buy and sell your products.
-                    </p>
-                </div>
+    <section class="about">
+        <div class="about__container">
+            <div class="about__title">
+                <h2>Welcome to Our Ecommerence shop</h2>
             </div>
-        </section>
+            <div class="about__subtitle">
+                <h3>shopping made easy </h3>
+            </div>
+            <div class="about__info">
+                <p>We are simply an online store , looking forward to make
+                    shopping easy for people with in Uganda and beyond.
+                    <em>Shop</em> is a platform where you can buy and sell your products.
+                </p>
+            </div>
+        </div>
+    </section>
 
     <!--Featured-->
     <section class="featured">
@@ -65,34 +74,85 @@ require_once '../includes/products.php';
             </div>
         </div>
     </section>
-
-    <!--team-->
-    <section class="team">
-        < class="team__container">
-            <div class="team__title">
-                <h1>Our Team</h1>
+     <!-- main products section 4x4-->
+     <section class="products">
+        <div class="products__container">
+            <div class="products__title">
+                <h1>Products</h1>
             </div>
-            <div class="team__members">
-                <!-- loop through the team members  -->
-                <? foreach ($team as $member) : ?>
-                    <div class="team__member">
-                        <div class="team__member__image">
-                            <img src="images/<?php echo $member['image']; ?>" alt="">
+            <div class="products__grid">
+                <!-- loop through the rest of the products -->
+                <?php foreach ($products as $product) : ?>
+                    <div class="products__grid__item">
+                        <div class="products__grid__item__image">
+                            <img src="<?php echo base_url($product['image']) ?>" alt="">
                         </div>
-                        <div class="team__member__info">
-                            <h3><?php echo $member['name']; ?></h3>
-                            <p><?php echo $member['position']; ?></p>
-                            <p><?php echo $member['description']; ?></p>
+                        <div class="products__grid__item__info">
+                            <h3><?php echo $product['name']; ?></h3>
+                            <p><?php echo $product['description']; ?></p>
+                            <p><?php echo $product['price']; ?></p>
+                            <a href="product.php?id=<?php echo $product['id']; ?>">View</a>
                         </div>
                     </div>
                 <?php endforeach; ?>
             </div>
+            <!-- pagination -->
+            <div class="pagination">
+                <a href="#" class="pagination__link">1</a>
+                <a href="#" class="pagination__link">2</a>
+                <a href="#" class="pagination__link">3</a>
+                <a href="#" class="pagination__link">4</a>
+                <a href="#" class="pagination__link">5</a>
+            </div>
         </div>
     </section>
 
-    
-     <!-- footer -->
-     <footer class="footer">
+    <!--team-->
+    <section class="team">
+        <div class="team__container">
+            <div class="team__title">
+                <h1>Our Team</h1>
+            </div>
+            <div class="team__members">
+                <div class="team__member">
+                    <div class="team__member__image">
+                        <img src="images/ass2.jpg" alt="">
+                    </div>
+                    <div class="team__member__info">
+                        <h3>Human  Resource </h3>
+                        <h6>MARGET</h6>
+                        <p>marget is the human resource of shop</p>
+
+                    </div>
+                </div>
+                <div class="team__member">
+                    <div class="team__member__image">
+                        <img src="images/ass3.jpg" alt="">
+                    </div>
+                    <div class="team__member__info">
+                        <h3>CEO</h3>
+                        <h6>Claire </h6>
+                        <p>Claire is the chief executive officer of shop</p>
+
+                    </div>
+                </div>
+                <div class = "team__member">
+                    <div class = "team__member__image">
+                        <img src = "images/ass5.jpg" alt = "">
+                    </div>
+                    <div class = "team__member__info">
+                        <h3>Marketing Manager</h3>
+                        <h6>Sara</h6>
+                        <p>Sara is the marketing manager of shop</p>
+                    </div>
+
+                </div>
+            </div>
+    </section>
+
+
+    <!-- footer -->
+    <footer class="footer">
         <div class="footer__container">
             <div class="footer__links">
                 <div class="footer__links__item">
@@ -136,6 +196,4 @@ require_once '../includes/products.php';
             </div>
         </div>
     </footer>
-<?php require_once '../partials/footer.php'; ?>
-
-
+    <?php require_once '../partials/footer.php'; ?>
