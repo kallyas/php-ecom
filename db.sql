edefbb344bf1a3c68506dcaecf0c5f8f095e0e1c
kallyas <kallyasmedia@gmail.com>
2022-12-02 11:30:36 +0300

    fix: errors

--- db.sql
+++ db.sql
@@ -76,15 +76,6 @@ CREATE TABLE IF NOT EXISTS `cart_items` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
---------------------------------------------------------
-
-CREATE TABLE IF NOT EXITS `TEAM` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
-  `description` text NOT NULL,
-  `parent_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
